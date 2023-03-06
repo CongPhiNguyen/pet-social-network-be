37707d39482a03fd127a6cb24cd575f3ec5b60b5
CongPhiNguyen <congphinguyen312@gmail.com>
2023-03-07 00:15:54 +0700

    basic totp with bugs

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -1,128 +1,218 @@
-const Users = require('../models/userModel')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const Users = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const speakeasy = require("speakeasy")
 
 const authCtrl = {
-    register: async (req, res) => {
-        try {
-            const { fullname, username, email, password, gender } = req.body
-            let newUserName = username.toLowerCase().replace(/ /g, '')
+  register: async (req, res) => {
+    try {
+      const { fullname, username, email, password, gender } = req.body
+      let newUserName = username.toLowerCase().replace(/ /g, "")
 
-            const user_name = await Users.findOne({ username: newUserName })
-            if (user_name) return res.status(400).json({ msg: "This user name already exists." })
+      const user_name = await Users.findOne({ username: newUserName })
+      if (user_name)
+        return res.status(400).json({ msg: "This user name already exists." })
 
-            const user_email = await Users.findOne({ email })
-            if (user_email) return res.status(400).json({ msg: "This email already exists." })
+      const user_email = await Users.findOne({ email })
+      if (user_email)
+        return res.status(400).json({ msg: "This email already exists." })
 
-            if (password.length < 6)
-                return res.status(400).json({ msg: "Password must be at least 6 characters." })
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: "Password must be at least 6 characters." })
 
-            const passwordHash = await bcrypt.hash(password, 12)
+      const passwordHash = await bcrypt.hash(password, 12)
 
-            const newUser = new Users({
-                fullname, username: newUserName, email, password: passwordHash, gender
-            })
+      const newUser = new Users({
+        fullname,
+        username: newUserName,
+        email,
+        password: passwordHash,
+        gender
+      })
 
+      const access_token = createAccessToken({ id: newUser._id })
+      const refresh_token = createRefreshToken({ id: newUser._id })
 
-            const access_token = createAccessToken({ id: newUser._id })
-            const refresh_token = createRefreshToken({ id: newUser._id })
+      res.cookie("refreshtoken", refresh_token, {
+        httpOnly: true,
+        path: "/api/refresh_token",
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30days
+      })
 
-            res.cookie('refreshtoken', refresh_token, {
-                httpOnly: true,
-                path: '/api/refresh_token',
-                maxAge: 30 * 24 * 60 * 60 * 1000 // 30days
-            })
+      await newUser.save()
 
-            await newUser.save()
-
-            res.json({
-                msg: 'Register Success!',
-                access_token,
-                user: {
-                    ...newUser._doc,
-                    password: ''
-                }
-            })
-        } catch (err) {
-            return res.status(500).json({ msg: err.message })
+      res.json({
+        msg: "Register Success!",
+        access_token,
+        user: {
+          ...newUser._doc,
+          password: ""
         }
-    },
-    login: async (req, res) => {
-        try {
-            const { email, password } = req.body
-            console.log(email)
-            console.log(password)
-            const user = await Users.findOne({ email })
-                .populate("followers following", "avatar username fullname followers following")
-
-            if (!user) return res.status(400).json({ msg: "This email does not exist." })
-
-            const isMatch = await bcrypt.compare(password, user.password)
-            if (!isMatch) return res.status(400).json({ msg: "Password is incorrect." })
-
-            const access_token = createAccessToken({ id: user._id })
-            const refresh_token = createRefreshToken({ id: user._id })
-
-            res.cookie('refreshtoken', refresh_token, {
-                httpOnly: true,
-                path: '/api/refresh_token',
-                maxAge: 30 * 24 * 60 * 60 * 1000 // 30days
-            })
-
-            res.json({
-                msg: 'Login Success!',
-                access_token,
-                user: {
-                    ...user._doc,
-                    password: ''
-                }
-            })
-        } catch (err) {
-            return res.status(500).json({ msg: err.message })
-        }
-    },
-    logout: async (req, res) => {
-        try {
-            res.clearCookie('refreshtoken', { path: '/api/refresh_token' })
-            return res.json({ msg: "Logged out!" })
-        } catch (err) {
-            return res.status(500).json({ msg: err.message })
-        }
-    },
-    generateAccessToken: async (req, res) => {
-        try {
-            const rf_token = req.cookies.refreshtoken
-            if (!rf_token) return res.status(400).json({ msg: "Please login now." })
-
-            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async (err, result) => {
-                if (err) return res.status(400).json({ msg: "Please login now." })
-
-                const user = await Users.findById(result.id).select("-password")
-                    .populate('followers following', 'avatar username fullname followers following')
-
-                if (!user) return res.status(400).json({ msg: "This does not exist." })
-
-                const access_token = createAccessToken({ id: result.id })
-
-                res.json({
-                    access_token,
-                    user
-                })
-            })
-
-        } catch (err) {
-            return res.status(500).json({ msg: err.message })
-        }
+      })
+    } catch (err) {
+      return res.status(500).json({ msg: err.message })
     }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body
+      console.log(email)
+      console.log(password)
+      const user = await Users.findOne({ email }).populate(
+        "followers following",
+        "avatar username fullname followers following"
+      )
+
+      if (!user)
+        return res.status(400).json({ msg: "This email does not exist." })
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch)
+        return res.status(400).json({ msg: "Password is incorrect." })
+
+      const access_token = createAccessToken({ id: user._id })
+      const refresh_token = createRefreshToken({ id: user._id })
+
+      res.cookie("refreshtoken", refresh_token, {
+        httpOnly: true,
+        path: "/api/refresh_token",
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30days
+      })
+
+      res.json({
+        msg: "Login Success!",
+        access_token,
+        user: {
+          ...user._doc,
+          password: ""
+        }
+      })
+    } catch (err) {
+      return res.status(500).json({ msg: err.message })
+    }
+  },
+  logout: async (req, res) => {
+    try {
+      res.clearCookie("refreshtoken", { path: "/api/refresh_token" })
+      return res.json({ msg: "Logged out!" })
+    } catch (err) {
+      return res.status(500).json({ msg: err.message })
+    }
+  },
+  generateAccessToken: async (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken
+      if (!rf_token) return res.status(400).json({ msg: "Please login now." })
+
+      jwt.verify(
+        rf_token,
+        process.env.REFRESH_TOKEN_SECRET,
+        async (err, result) => {
+          if (err) return res.status(400).json({ msg: "Please login now." })
+
+          const user = await Users.findById(result.id)
+            .select("-password")
+            .populate(
+              "followers following",
+              "avatar username fullname followers following"
+            )
+
+          if (!user)
+            return res.status(400).json({ msg: "This does not exist." })
+
+          const access_token = createAccessToken({ id: result.id })
+
+          res.json({
+            access_token,
+            user
+          })
+        }
+      )
+    } catch (err) {
+      return res.status(500).json({ msg: err.message })
+    }
+  },
+  generateOTP: async (req, res) => {
+    const { userId, email } = req.body
+    // TODO: userId not found
+    const { ascii, hex, base32, otpauth_url } = speakeasy.generateSecret({
+      issuer: userId,
+      name: email,
+      length: 15
+    })
+    const userUpdateOTP = await Users.findByIdAndUpdate(userId, {
+      otpInfo: {
+        otp_ascii: ascii,
+        otp_auth_url: otpauth_url,
+        otp_base32: base32,
+        otp_hex: hex
+      }
+    }).lean()
+    console.log(userUpdateOTP)
+    res.status(200).json({
+      success: true,
+      base32,
+      otpauth_url
+    })
+  },
+  verifyOTP: async (req, res) => {
+    const { userId, token } = req.body
+    const user = await Users.findById(userId)
+    console.log(user)
+    const message = "Token is invalid or user doesn't exist"
+    if (!user) {
+      return res.status(401).json({
+        status: "fail",
+        message
+      })
+    }
+
+    console.log(user?.otpInfo?.otp_base32)
+
+    const verified = speakeasy.totp.verify({
+      secret: user?.otpInfo?.otp_base32,
+      encoding: "base32",
+      token
+    })
+
+    console.log(verified)
+
+    if (!verified) {
+      return res.status(401).json({
+        status: "fail",
+        message
+      })
+    }
+
+    const updatedUser = Users.findByIdAndUpdate(userId, {
+      otpInfo: {
+        otp_enabled: true,
+        otp_verified: true
+      }
+    })
+
+    res.status(200).json({
+      otp_verified: true,
+      user: {
+        id: updatedUser.id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        otp_enabled: updatedUser.otpInfo.otp_enabled
+      }
+    })
+  }
 }
 
-
 const createAccessToken = (payload) => {
-    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
 }
 
 const createRefreshToken = (payload) => {
-    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' })
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "30d"
+  })
 }
 
 module.exports = authCtrl
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,13 +1,15 @@
-const router = require('express').Router()
-const authCtrl = require('../controllers/authCtrl')
+const router = require("express").Router()
+const authCtrl = require("../controllers/authCtrl")
 
-router.post('/register', authCtrl.register)
+router.post("/register", authCtrl.register)
 
-router.post('/login', authCtrl.login)
+router.post("/login", authCtrl.login)
 
-router.post('/logout', authCtrl.logout)
+router.post("/logout", authCtrl.logout)
 
-router.post('/refresh_token', authCtrl.generateAccessToken)
+router.post("/refresh_token", authCtrl.generateAccessToken)
 
+router.post("/auth/otp/generate", authCtrl.generateOTP)
+router.post("/auth/otp/verify", authCtrl.verifyOTP)
 
 module.exports = router
