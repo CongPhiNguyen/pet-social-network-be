ecbca9d62727ff4d8b252408f595fee30aeb73e9
CongPhiNguyen <congphinguyen312@gmail.com>
2023-05-13 23:58:48 +0700

    delete console.log

--- controllers/petCtrl.js
+++ controllers/petCtrl.js
@@ -2,7 +2,6 @@ const Pets = require("../models/petModel")
 
 const petCtrl = {
   add: async (req, res) => {
-    console.log(req.body)
     const newPet = new Pets(req.body)
     const resSave = await newPet.save()
     res.status(200).send({ success: true, resSave })
